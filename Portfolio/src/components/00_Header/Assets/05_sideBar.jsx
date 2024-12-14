89532e61a667794287ce71a2174d9abb6c7b7239
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-12-14 14:59:28 +0530

    Fixed some changes

--- Portfolio/src/components/00_Header/Assets/05_sideBar.jsx
+++ Portfolio/src/components/00_Header/Assets/05_sideBar.jsx
@@ -11,13 +11,13 @@ const menuItems = [
 export function ResponsiveSidebar({ isSidebarOpen, toggleSidebar }) {
   return (
    <aside className={`fixed h-16 w-full bottom-0 md:top-[25%] md:h-[400px] md:w-16 md:rounded-md bg-gray-800 transform ${
-   isSidebarOpen ? 'translate-y-0 translate-x-0' : 'translate-y-0 translate-x-full md:hidden' 
+   isSidebarOpen ? 'translate-y-0 translate-x-full md:hidden' : 'translate-y-0 translate-x-0'
    }`}>
 
 <nav className='flex p-2 items-center justify-evenly md:flex-col md:gap-3 '>
   {menuItems.map((item) => {
     return (
-      <div className='flex flex-col items-center cursor-pointer' key={item.name}>
+      <div className='flex flex-col items-center cursor-pointer hover:text-gray-300' key={item.name}>
         <span>{item.icon}</span>
         <span>{item.name}</span>
       </div>
