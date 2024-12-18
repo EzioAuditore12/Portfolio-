a291a3b1747edb05488c4991face608cd3598a30
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-12-18 23:00:06 +0530

    Added profile pic and updated it in about section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import Header from './components/00_Header/Header'
-
+import About from './components/01_About/About'
 function App() {
   return (
     <>
     <Header/>
+    <div className='flex justify-center items-center'>
+    <About/>
+    </div>
+    
     </>
   )
 }
--- Portfolio/src/components/01_About/About.jsx
+++ Portfolio/src/components/01_About/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import profilePic from './Assets/profileImage.jpg'
 function About() {
   return (
-    <div>About</div>
+    <div className= 'h-[600px] w-[80vw] flex flex-col items-center border-b-2 md:mt-[60px] border-light-text dark:border-dark-text bg-light-primary dark:bg-[#161D29] text-light-text dark:text-dark-text'>
+    <div className='h-[180px] w-[180px] rounded-[900px] border-[10px] border-white dark:border-gray-700 absolute top-8 md:top-[70px]'>
+    <img src={profilePic} className='object-contain rounded-[900px]'/>
+    </div>
+     
+    </div>
   )
 }
 
