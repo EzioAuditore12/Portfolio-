7289cde985afbc6df3823bf0ec5fc6f120174307
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-12-22 15:51:52 +0530

    Fixed rsponsiveness issue

--- Portfolio/src/components/01_About/About.jsx
+++ Portfolio/src/components/01_About/About.jsx
@@ -4,7 +4,7 @@ import Section2 from './02_Section2'
 import Section3 from './03_Section3'
 function About() {
   return (
-    <div className='grid border-b-7 border-black gap-5 mb-[100px] md:mb-3'>
+    <div className='grid border-b-7 border-black gap-5 mb-[100px] h-sm:mb-[100px] md:mb-3'>
     <Section1/>
     <Section2/>
     <Section3/>
