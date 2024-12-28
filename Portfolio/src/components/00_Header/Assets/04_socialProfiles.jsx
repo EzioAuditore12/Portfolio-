8bed726570a4ce0e7ad47a181a220a0228a115c3
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-12-28 19:18:38 +0530

    Changed navbar Icons

--- Portfolio/src/components/00_Header/Assets/04_socialProfiles.jsx
+++ Portfolio/src/components/00_Header/Assets/04_socialProfiles.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Github from './Svg/02_SocialProfileIcons/01_Github.svg';
-import Linkedin from './Svg/02_SocialProfileIcons/02_Linkedin.svg';
-import StackOverflow from './Svg/02_SocialProfileIcons/03_StackOverflow.svg';
-import Reddit from './Svg/02_SocialProfileIcons/04_Reddit.svg';
-import Twitter from './Svg/02_SocialProfileIcons/05_Twitter.svg';
+import { FaGithub, FaLinkedin, FaStackOverflow, FaReddit, FaTwitter } from 'react-icons/fa';
 
 const socialProfiles = [
-  { name: 'Github', href: 'https://github.com/EzioAuditore12', icon: Github },
-  { name: 'Linkedin', href: 'https://www.linkedin.com/in/daksh-purohit-0230372a1/', icon: Linkedin },
-  { name: 'StackOverflow', href: 'https://stackoverflow.com/users/28030439/kin-kaneki', icon: StackOverflow },
-  { name: 'Reddit', href: 'https://www.reddit.com/user/LongjumpingDoubt5206/', icon: Reddit },
-  { name: 'Twitter', href: 'https://x.com/LvxSasuke', icon: Twitter }
+  { name: 'Github', href: 'https://github.com/EzioAuditore12', icon: FaGithub },
+  { name: 'Linkedin', href: 'https://www.linkedin.com/in/daksh-purohit-0230372a1/', icon: FaLinkedin },
+  { name: 'StackOverflow', href: 'https://stackoverflow.com/users/28030439/kin-kaneki', icon: FaStackOverflow },
+  { name: 'Reddit', href: 'https://www.reddit.com/user/LongjumpingDoubt5206/', icon: FaReddit },
+  { name: 'Twitter', href: 'https://x.com/LvxSasuke', icon: FaTwitter }
 ];
 
 function SocialProfiles() {
@@ -18,8 +14,14 @@ function SocialProfiles() {
     <div className='hidden md:block mr-7 mx-auto'>
       <div className='flex items-center justify-center gap-2'>
         {socialProfiles.map((profile, index) => (
-          <a key={index} href={profile.href} target="_blank" rel="noopener noreferrer">
-            <img className='h-[35px] w-[40px]' src={profile.icon} alt={profile.name} />
+          <a 
+            key={index} 
+            href={profile.href} 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-500 transition-colors"
+          >
+            <profile.icon size={30} />
           </a>
         ))}
       </div>
--- Portfolio/src/components/00_Header/Assets/05_sideBar.jsx
+++ Portfolio/src/components/00_Header/Assets/05_sideBar.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
-import userIcon from './Svg/03_SideBarIcons/01_userIcon.svg';
-import projectIcon from './Svg/03_SideBarIcons/02_projectIcon.svg';
-import blogIcon from './Svg/03_SideBarIcons/03_blogIcon.svg';
-import resumeIcon from './Svg/03_SideBarIcons/04_resumeIcon.svg';
-import contactIcon from './Svg/03_SideBarIcons/05_contactIcon.svg';
 import { Link } from 'react-router-dom';
+import { FaUser, FaCode, FaBlog, FaFileAlt, FaEnvelope } from 'react-icons/fa';
+
 const menuItems = [
-  { name: 'About', link: '/about', icon: userIcon},
-  { name: 'Projects', link: '/projects', icon: projectIcon },
-  { name: 'Blog', link: '/blog', icon: blogIcon },
-  { name: 'Resume', link: '/resume', icon: resumeIcon},
-  { name: 'Contact', link: '/contact', icon: contactIcon},
+  { name: 'About', link: '/about', icon: <FaUser size={24} /> },
+  { name: 'Projects', link: '/projects', icon: <FaCode size={24} /> },
+  { name: 'Blog', link: '/blog', icon: <FaBlog size={24} /> },
+  { name: 'Resume', link: '/resume', icon: <FaFileAlt size={24} /> },
+  { name: 'Contact', link: '/contact', icon: <FaEnvelope size={24} /> },
 ];
 
 export function ResponsiveSidebar({ isSidebarOpen, toggleSidebar }) {
   return (
-   <aside className={` bg-light-primary dark:bg-[#161D29] text-light-text dark:text-dark-text shadow-[0_0_10px_rgba(0,0,0,0.3)] fixed h-16 w-full bottom-0 z-10 h-sm:h-16 h-sm:w-full h-sm:bottom-0 h-sm:top-auto h-sm:ml-0 h-sm:rounded-none md:top-[25%] md:h-[400px] md:w-16 md:ml-2 md:rounded-lg transform ${
-   isSidebarOpen ? 'translate-y-0 translate-x-full md:hidden' : 'translate-y-0 translate-x-0'
-   }`}>
-
-<nav className='flex p-2 items-center justify-evenly md:flex-col md:gap-3 h-sm:flex-row'>
-  {menuItems.map((item) => (
-    <Link to={item.link} key={item.name} className='flex flex-col items-center cursor-pointer hover:text-gray-300'>
-      <img className='h-[30px] w-[30px]' src={item.icon} alt={item.name} />
-      <span>{item.name}</span>
-    </Link>
-  ))}
-  <button onClick={toggleSidebar} className="hidden h-sm:hidden md:block hover:text-gray-300">
-            ✕
-  </button>
-</nav>  
-   </aside>
+    <aside className={`bg-light-primary dark:bg-[#161D29] text-light-text dark:text-dark-text shadow-[0_0_10px_rgba(0,0,0,0.3)] fixed h-16 w-full bottom-0 z-10 h-sm:h-16 h-sm:w-full h-sm:bottom-0 h-sm:top-auto h-sm:ml-0 h-sm:rounded-none md:top-[25%] md:h-[400px] md:w-16 md:ml-2 md:rounded-lg transform ${
+      isSidebarOpen ? 'translate-y-0 translate-x-full md:hidden' : 'translate-y-0 translate-x-0'
+    }`}>
+      <nav className='flex p-2 items-center justify-evenly md:flex-col md:gap-3 h-sm:flex-row'>
+        {menuItems.map((item) => (
+          <Link to={item.link} key={item.name} className='flex flex-col items-center cursor-pointer hover:text-gray-300'>
+            {item.icon}
+            <span>{item.name}</span>
+          </Link>
+        ))}
+        <button onClick={toggleSidebar} className="hidden h-sm:hidden md:block hover:text-gray-300">
+          ✕
+        </button>
+      </nav>  
+    </aside>
   );
 }
