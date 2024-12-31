1856eaba2c61f3b548d5d45318d5e644472f479b
Daksh Purohit <115918398+EzioAuditore12@users.noreply.github.com>
2024-12-31 14:45:39 +0530

    Section 1 final done

--- Portfolio/src/components/01_About/02_Section2.jsx
+++ Portfolio/src/components/01_About/02_Section2.jsx
@@ -3,28 +3,52 @@ import ProjectCard from '../../shared/01_projectCard/projectCard'
 import project1Img from './Assets/projectImages/ByteBoulevardProject.png'
 import project2Img from './Assets/projectImages/AnimeImage.png'
 import project3Img from './Assets/projectImages/SpanHamDetection.png'
+import project4Img from './Assets/projectImages/INKALCHEMY.png'
 import '../../styles/sectionAnimations.css'
-
+import { useState,useEffect } from 'react'
 function Section2() {
   const Projects = [
     { 
       name: 'Ecommerce', 
       href: '#', 
       img: project1Img, 
-      psummary: 'This is the frontend of a ecommerece store using various famous webtools like react'
+      psummary: 'This is the frontend of a ecommerece store using various famous webtools like react',
+      websiteLink:'http://13.232.83.157:8256/'
     },
     {
       name: 'Spam Ham Detection',
       img: project3Img,
-      psummary: "This is a project to find whether a message is spam or ham"
+      psummary: "This is a project to find whether a message is spam or ham",
+       websiteLink:'https://spam-ham-detection-manas.onrender.com/'
     },
     {
       name: 'Anime Website',
       img: project2Img,
-      psummary: "It is a website that showcases use of bootstrap to build websites"
+      psummary: "It is a website that showcases use of bootstrap to build websites",
+       websiteLink:'http://13.232.83.157:8257/'
     },
+    {
+      name: 'Ink Alchemy',
+      img: project4Img,
+      psummary: 'A website to use various AWS functionalities such as translate,senimental analysis and image to text',
+       websiteLink:'http://13.232.83.157:5173/'
+    }
   ];
-  
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Get products based on screen size
+  const displayedProducts = isMobile ? Projects.slice(0, 3) : Projects;
+
   return (
     <div className='sectionCSS p-6 section-fade-in delay-0 '>
       <h1 className='text-3xl font-extrabold'>Featured Projects</h1>
@@ -32,12 +56,13 @@ function Section2() {
         Here are some of my projects that I have worked on. Click on the project to view more details.
       </p>
       <div className='grid place-items-center gap-y-4 md:grid-cols-2'>
-        {Projects.map((item, index) => (
+        {displayedProducts.map((item, index) => (
           <ProjectCard 
             key={index}
             projectTitle={item.name} 
             projectImage={item.img}
             projectSummary={item.psummary}
+            websiteLink={item.websiteLink}
           />
         ))}
       </div>
--- Portfolio/src/shared/01_projectCard/projectCard.jsx
+++ Portfolio/src/shared/01_projectCard/projectCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
 function ProjectCard({
-    projectTitle,
-    projectImage,
-    projectSummary
+  projectTitle,
+  projectImage,
+  projectSummary,
+  websiteLink
 }) {
-  return (
-    <div className='w-[95%] grid h-full rounded-md overflow-hidden'>
-        <img className='object-center h-[260px] w-full sm:h-[330px] md:h-[250px] lg:h-[300px] xl:h-[450px]' src={projectImage}/>
-        <h2 className='text-start'>{projectTitle}</h2>
-        <p>{projectSummary}</p>
-        <span className='text-blue-500 cursor-pointer'>View more &rarr;</span>
-    </div>
-  )
+return (
+  <div className='w-[95%] grid h-full rounded-md overflow-hidden'>
+      <img className='object-center h-[260px] w-full sm:h-[330px] md:h-[250px] lg:h-[300px] xl:h-[450px]' src={projectImage}/>
+      <h2 className='text-start'>{projectTitle}</h2>
+      <p>{projectSummary}</p>
+      <span className='text-blue-500 cursor-pointer'>
+        <a href={websiteLink} target="_blank" rel="noopener noreferrer">View Website &rarr;</a>
+      </span>
+  </div>
+)
 }
 
 export default ProjectCard
